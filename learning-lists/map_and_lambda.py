f0e7485d5db6aac87a8b7cc81b8acaefc2829138
dário junio <dariojunio.work@gmail.com>
2026-03-09 13:46:57 -0300

    uploading my studies
    Add a script to demonstrate mapping with a lambda function.

---
+++ learning-lists/map_and_lambda.py
@@ -0,0 +1,7 @@
+print(list(map(lambda x: x + 20, [20, 40, 60, 80])))
+
+#1.0 imprimir // print()
+#1.1 converter em lista // list()
+#1.2 funcao map que retorna em objeto // map()
+#1.3 funcao lambda // lambda x: x + 20,
+#1.4 lista mapeada a funcao lambda // [20, 40, 60, 80]
