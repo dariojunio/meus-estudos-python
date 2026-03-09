75d610d3d868479f9c94c9d63a511c021190d9f4
dário junio <dariojunio.work@gmail.com>
2026-03-09 11:34:30 -0300

    uploading my studies

---
+++ learning-lists/using_list.py
@@ -0,0 +1,12 @@
+#usando list
+
+var = list('papalelepipedo')
+print(var)
+
+#usando list em duas listas e juntando elas
+
+valor = [40, 20, 25]
+brinquedo = ['carro', 'moto', 'bola']
+
+juncao = zip(brinquedo, valor)
+print(list(juncao))
